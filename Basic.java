0386c672b9085ffad6807ab7ea89b93399aad25d
sam-349 <114975742+sam-349@users.noreply.github.com>
2023-02-20 00:39:50 +0530

    default values in java

--- Basic.java
+++ Basic.java
@@ -5,6 +5,8 @@ class Baisc{
     static String str; 
     static float f;
     static double d;    
+    // if variables declared inside the main then program gives an error
+    // error: variable ** might not have been initialized. ** represents variable name.
     public static void main(String args[]){
         System.out.println("Integer: "+i);
         System.out.println("Long: "+l);
