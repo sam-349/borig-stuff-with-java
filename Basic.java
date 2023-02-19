dcc73403796afabea7f29bf450a126df46f0b5fa
sam <samuelraj349@gmail.com>
2023-02-19 12:44:39 +0530

    default values

--- Basic.java
+++ Basic.java
@@ -1,7 +1,16 @@
-import java.lang.*;
-
-class Basic{
+class Baisc{
+    static int i;
+    static long l;    
+    static char ch;
+    static String str; 
+    static float f;
+    static double d;    
     public static void main(String args[]){
-        System.out.println("Hello World!");
+        System.out.println("Integer: "+i);
+        System.out.println("Long: "+l);
+        System.out.println("char: "+ch);
+        System.out.println("String: "+str);
+        System.out.println("Float: "+f);
+        System.out.println("Double: "+d);
     }
 }
